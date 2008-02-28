fcd01ddb9365f52fbafce54b69cd7e33c8511394
Andy Armstrong <andy@hexten.net>
2008-02-28 23:11:49 +0000

    Added
    git-svn-id: https://svn.hexten.net/andy/home@3362 7993f63a-630d-0410-9861-a193ae2ae3d1

--- .test-runner/config.pl
+++ .test-runner/config.pl
@@ -10,8 +10,31 @@ sub find_perls {
 set_config(
     dists => [
         'HTML::Tiny' => {
-            svn =>
-              'http://svn.hexten.net/andy/Perl/HTML/HTML-Tiny/trunk'
+            source => {
+                type => 'svn',
+                url =>
+                  'http://svn.hexten.net/andy/Perl/HTML/HTML-Tiny/trunk',
+            },
+            scripts => ['makefile_pl'],
         }
     ],
+    scripts => {
+        makefile_pl => {
+            precondition => sub {
+                -f 'Makefile.PL';
+            },
+            steps => [
+                '%PERL% Makefile.PL',
+                'make',
+                'make test',
+                'make distclean',
+            ],
+        },
+    },
+    env => {
+        PERL => {
+            class => 'Test::SmokeStack::Perl',
+
+        },
+    }
 );
