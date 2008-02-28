5315142f568a86c02c6b96c7531fd9f6b50900b8
Andy Armstrong <andy@hexten.net>
2008-02-28 01:36:46 +0000

    Added
    git-svn-id: https://svn.hexten.net/andy/home@3361 7993f63a-630d-0410-9861-a193ae2ae3d1

---
+++ .test-runner/config.pl
@@ -0,0 +1,17 @@
+# config.pl
+
+use strict;
+use warnings;
+
+sub find_perls {
+
+}
+
+set_config(
+    dists => [
+        'HTML::Tiny' => {
+            svn =>
+              'http://svn.hexten.net/andy/Perl/HTML/HTML-Tiny/trunk'
+        }
+    ],
+);
