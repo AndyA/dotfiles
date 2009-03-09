864b1116bc6326f5670466402dc5b97e9b0d11e8
Andy Armstrong <andy@hexten.net>
2009-03-09 20:10:59 +0000

    Tagging 0.01
    git-svn-id: https://svn.hexten.net/andy/home@4495 7993f63a-630d-0410-9861-a193ae2ae3d1

---
+++ testproject/Foo-Bar/tags/0.01/trunk/lib/Foo/Bar.pm
@@ -0,0 +1,62 @@
+package Foo::Bar;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Foo::Bar - Just a test.
+
+=head1 VERSION
+
+This document describes Foo::Bar version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Foo::Bar;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< somfunc >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+Foo::Bar requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-foo-bar@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2009, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
