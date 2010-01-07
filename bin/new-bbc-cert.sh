957a63c89cfdbf13171041128cdc9044da94c6a0
Andy Armstrong <andy@hexten.net>
2010-01-07 22:56:14 +0000

    Added
    git-svn-id: https://svn.hexten.net/andy/home@4945 7993f63a-630d-0410-9861-a193ae2ae3d1

---
+++ bin/new-bbc-cert.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+STORE=~/.keystore
+DROP=~/Desktop/andy.p12
+PKCS12=$STORE/andy.p12
+CRT=$STORE/andy.crt
+KEY=$STORE/andy.key
+
+cp $DROP $PKCS12
+openssl pkcs12 -in $PKCS12 -out $KEY -nodes -clcerts -nocerts
+openssl pkcs12 -in $PKCS12 -out $CRT -nodes -clcerts -nokeys
+
+# Copy some other places
+cp $PKCS12 ~/.subversion/certs
+sudo cp $CRT $KEY /opt/local/etc/stunnel
+sudo chown root:admin /opt/local/etc/stunnel/andy.{crt,key}
+sudo chmod 400 /opt/local/etc/stunnel/andy.{crt,key}
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
