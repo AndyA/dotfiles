9b70dfc65bfed3792e6376fc2cc282630c09eb69
Andy Armstrong <andy@hexten.net>
2010-03-28 13:35:39 +0000

    Script to build the latest version of SVN.
    git-svn-id: https://svn.hexten.net/andy/home@4982 7993f63a-630d-0410-9861-a193ae2ae3d1

---
+++ bin/get-and-build-svn.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+VERSION=1.6.9
+PREFIX=/alt/local
+TEMP="/tmp/svn.$$"
+HERE=$(pwd)
+cd $TEMP
+sudo apt-get update
+sudo apt-get install build-essential openssl ssh expat libxyssl-dev libssl-dev
+sudo apt-get remove subversion
+sudo dpkg --purge subversion
+wget http://subversion.tigris.org/downloads/subversion-$VERSION.tar.gz
+wget http://subversion.tigris.org/downloads/subversion-deps-$VERSION.tar.gz
+tar xvfz subversion-$VERSION.tar.gz
+tar xvfz subversion-deps-$VERSION.tar.gz
+cd subversion-$VERSION/neon/
+./configure --prefix=$PREFIX --with-ssl --with-pic
+make
+sudo make install
+cd ..
+rm -rf neon
+./configure --prefix=$PREFIX --with-ssl --with-neon=$PREFIX
+make
+sudo make install
+cd $HERE
+rm -rf $TEMP
+exit 0
