531f43aaa6ec0b86d36503b3e0772db7bbbf55a5
Andy Armstrong <andy@hexten.net>
2011-01-07 21:28:44 +0000

    Look around for stunnel config.

--- bin/new-bbc-cert.sh
+++ bin/new-bbc-cert.sh
@@ -6,15 +6,19 @@ PKCS12=$STORE/andy.p12
 CRT=$STORE/andy.crt
 KEY=$STORE/andy.key
 
+STUNNEL_CFG=/etc/stunnel
+[ -d /usr/local/etc/stunnel ] && STUNNEL_CFG=/usr/local/etc/stunnel
+[ -d /alt/local/etc/stunnel ] && STUNNEL_CFG=/alt/local/etc/stunnel
+
 cp $DROP $PKCS12
 openssl pkcs12 -in $PKCS12 -out $KEY -nodes -clcerts -nocerts
 openssl pkcs12 -in $PKCS12 -out $CRT -nodes -clcerts -nokeys
 
 # Copy some other places
 cp $PKCS12 ~/.subversion/certs
-sudo cp $CRT $KEY /alt/local/etc/stunnel
-sudo chown root:admin /alt/local/etc/stunnel/andy.{crt,key}
-sudo chmod 400 /alt/local/etc/stunnel/andy.{crt,key}
+sudo cp $CRT $KEY $STUNNEL_CFG
+sudo chown root:admin $STUNNEL_CFG/andy.{crt,key}
+sudo chmod 400 $STUNNEL_CFG/andy.{crt,key}
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
