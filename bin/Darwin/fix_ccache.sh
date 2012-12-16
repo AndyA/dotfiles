97ae9a35814d7fe984c2724c2b0ba18edf5c56e2
Andy Armstrong <andy@hexten.net>
2012-12-16 20:55:54 +0000

    Install ccache if necessary.
    Tidy trailing junk.

--- bin/Darwin/fix_ccache.sh
+++ bin/Darwin/fix_ccache.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-ccache="$( which ccache )"
+ccache="$( which ccache )" 
+if [ -z "$ccache" ]; then
+  brew install ccache || exit
+  ccache="$( which ccache )" 
+fi
 real="$( dirname "$ccache" )"/"$( readlink "$ccache" )"
 
 echo "$ccache -> $real"
@@ -12,21 +16,6 @@ for cc in c++ c++-3.3 c++-4.0 c++-4.2 c++3 cc \
   echo "$targ -> $real"
   ln -s "$real" "$targ"
 done
-exit
-
-
-
-
-[20:37] andy@voodoo:~/Works/Media/emitron/lib/pipetoys
-$ ls /usr/local/Cellar/ccache/
-3.1.8
-[20:37] andy@voodoo:~/Works/Media/emitron/lib/pipetoys
-$ ls -l /usr/local/bin/ccache 
-lrwxr-xr-x  1 andy  staff  33 16 Dec 20:37 /usr/local/bin/ccache -> ../Cellar/ccache/3.1.8/bin/ccache
-[20:38] andy@voodoo:~/Works/Media/emitron/lib/pipetoys
-$ readlink $( which ccache )
-../Cellar/ccache/3.1.8/bin/ccache
-
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
