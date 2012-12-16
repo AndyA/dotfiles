0925a4269916729bf3e0cbfd668b02dcd423fc75
Andy Armstrong <andy@hexten.net>
2012-12-16 20:35:34 +0000

    Handle filenames with spaces.

--- bin/fix_usr_local_perms.sh
+++ bin/fix_usr_local_perms.sh
@@ -4,7 +4,7 @@
 
 chown -R root:staff /usr/local
 chmod -R ug+rwX /usr/local
-find /usr/local -type d | xargs chmod g+s
+find /usr/local -type d -print0 | xargs -0 chmod g+s
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
