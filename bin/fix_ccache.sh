1bd67b77f1e7b25e72ccb4f94c68712281dcf183
Andy Armstrong <andy@hexten.net>
2012-12-22 19:51:45 +0000

    Try everything from 2.0 to 5.9.

--- bin/fix_ccache.sh
+++ bin/fix_ccache.sh
@@ -12,19 +12,20 @@ echo "Using $ccache"
 
 PATH=$( perl -e 'print join ":", grep !m{^/usr/local/bin}, split ":", $ENV{PATH}' )
 
-for cc in c++ c++-3.3 c++-4.0 c++-4.2 c++3 cc \
-  g++ g++-3.3 g++-4.0 g++-4.2 g++2 g++3 \
-  gcc gcc-3.3 gcc-4.0 gcc-4.2 gcc2 gcc3 \
-  clang; do
+skip=""
+for cc in {c++,g++,gcc,cc}{,-{2..5}{,.{0..9}}} clang; do
   targ="/usr/local/bin/$cc"
-  [ -e "$targ" ] && rm -f "$targ"
   if which "$cc" >/dev/null 2>&1; then
+    [ -e "$targ" ] && rm -f "$targ"
     echo "Linking $targ -> $ccache"
     ln -s "$ccache" "$targ"
   else
-    echo "No $cc, skipped"
+    skip="$skip $cc"
   fi
 done
+if [ "$skip" ]; then
+  echo "Not found:$skip"
+fi
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
