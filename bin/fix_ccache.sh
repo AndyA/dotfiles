f47c5a46c06eaeb270d43f14c38740ebbe08b0b5
Andy Armstrong <andy@hexten.net>
2012-12-22 19:44:01 +0000

    Add clang.

--- bin/fix_ccache.sh
+++ bin/fix_ccache.sh
@@ -14,7 +14,8 @@ PATH=$( perl -e 'print join ":", grep !m{^/usr/local/bin}, split ":", $ENV{PATH}
 
 for cc in c++ c++-3.3 c++-4.0 c++-4.2 c++3 cc \
   g++ g++-3.3 g++-4.0 g++-4.2 g++2 g++3 \
-  gcc gcc-3.3 gcc-4.0 gcc-4.2 gcc2 gcc3; do
+  gcc gcc-3.3 gcc-4.0 gcc-4.2 gcc2 gcc3 \
+  clang; do
   targ="/usr/local/bin/$cc"
   [ -e "$targ" ] && rm -f "$targ"
   if which "$cc" >/dev/null 2>&1; then
