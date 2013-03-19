316fde7050ceefaf4059eee7356ab294c16da68e
Andy Armstrong <andy@hexten.net>
2013-03-19 12:35:40 +0000

    Copy the right file.

--- bin/fix_perlbrew.sh
+++ bin/fix_perlbrew.sh
@@ -3,7 +3,7 @@
 [ -e ~/perl5/perlbrew/etc/bashrc ] || exit
 
 if diff ~/bin/perlbrew-0.60-bashrc.orig ~/perl5/perlbrew/etc/bashrc >/dev/null; then
-  cp ~/bin/perlbrew-0.60-bashrc.orig ~/perl5/perlbrew/etc/bashrc 
+  cp ~/bin/perlbrew-0.60-bashrc ~/perl5/perlbrew/etc/bashrc 
   echo "Fixed perlbrew bashrc"
 fi
 
