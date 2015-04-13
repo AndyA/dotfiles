2ec0c78bc7e89dc5bca85488dd341422f1afbe8c
Andy Armstrong <andy@hexten.net>
2015-04-13 23:02:20 +0100

    Installer fixed.

--- bin/Linux/install-todo.sh
+++ bin/Linux/install-todo.sh
@@ -4,9 +4,8 @@ repo='https://github.com/ginatrapani/todo.txt-cli.git'
 tmp='/tmp/install-todo.sh'
 bash_c_d='/etc/bash_completion.d'
 
-mkdir -p "$tmp"
+git clone "$repo" "$tmp"
 cd "$tmp"
-git clone $repo
 cp todo.sh /usr/local/bin/todo.sh
 chmod +x /usr/local/bin/todo.sh
 [ -d "$bash_c_d" ] && sudo cp todo_completion "$bash_c_d/todo"
