6c995a283bfde279080078d8fff78957242f8a9e
Andy Armstrong <andy@hexten.net>
2015-04-13 22:59:33 +0100

    Install/upgtade todo.txt on Linux

---
+++ bin/Linux/install-todo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+repo='https://github.com/ginatrapani/todo.txt-cli.git'
+tmp='/tmp/install-todo.sh'
+bash_c_d='/etc/bash_completion.d'
+
+mkdir -p "$tmp"
+cd "$tmp"
+git clone $repo
+cp todo.sh /usr/local/bin/todo.sh
+chmod +x /usr/local/bin/todo.sh
+[ -d "$bash_c_d" ] && sudo cp todo_completion "$bash_c_d/todo"
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
